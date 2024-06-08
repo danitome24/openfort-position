8fa6f03f1786d2bbd985e9fab8b0286c37adf31b
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-06-08 17:17:48 +0200

    Added some comments

--- script/DeployFakeTokens.s.sol
+++ script/DeployFakeTokens.s.sol
@@ -7,8 +7,8 @@ import {MockERC20} from "../src/mock/MockERC20.sol";
 contract DeployFakeTokens is Script {
     function run() external {
         vm.startBroadcast();
-        MockERC20 mockUsdcToken = new MockERC20("USDC Stable Token", "USDC");
-        MockERC20 mockMaticToken = new MockERC20("Matic Token", "MATIC");
+        MockERC20 mockUsdcToken = new MockERC20("USDC Stable Token", "USDC"); // Stablecoin.
+        MockERC20 mockMaticToken = new MockERC20("Matic Token", "MATIC"); // MATIC ERC20 example.
         vm.stopBroadcast();
 
         console.log("MATIC Token address %s", address(mockMaticToken));
