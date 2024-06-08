73b896a35396e7c9dc07504fca6c53644a538085
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-06-08 17:17:12 +0200

    Added constants in checkbalance script

--- script/CheckBalance.s.sol
+++ script/CheckBalance.s.sol
@@ -6,10 +6,13 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {MockERC20} from "../src/mock/MockERC20.sol";
 
 contract CheckBalance is Script {
+    address private constant TOKEN_TO_CHECK = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512; // MATIC.
+    address private constant ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
     function run() public {
         vm.startBroadcast();
-        IERC20 tokenBalance = MockERC20(0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512);
-        uint256 balance = tokenBalance.balanceOf(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        IERC20 tokenBalance = MockERC20(TOKEN_TO_CHECK);
+        uint256 balance = tokenBalance.balanceOf(ACCOUNT);
         vm.stopBroadcast();
 
         console.log(balance);
