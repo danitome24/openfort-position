51cd5eaec90fce64cc47277b9198dc2b7df67731
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-06-10 10:39:38 +0200

    Refactor in testing

--- test/OpenfortSwapperTest.t.sol
+++ test/OpenfortSwapperTest.t.sol
@@ -18,11 +18,15 @@ contract OpenfortSwapperTest is Test {
     address USER_ONE = makeAddr("user1");
     address USER_TWO = makeAddr("user2");
 
+    event StablecoinSendedToRecipient(address indexed to, uint256 amount);
+
     function setUp() external {
         address[] memory initialRecipients = new address[](1);
         initialRecipients[0] = USER_ONE;
         stablecoin = new MockStablecoin();
+
         swapRouter = new MockSwapRouter();
+        stablecoin.mint(address(swapRouter), 100);
         swapper = new OpenfortSwapper(
             initialRecipients, OpenfortSwapper.ShippingTime.Immediatly, INIT_FEES, swapRouter, address(stablecoin)
         );
@@ -52,24 +56,31 @@ contract OpenfortSwapperTest is Test {
 
     function testSwap() public {
         // Prepare
+        uint256 amountIn = 6;
         MockMaticToken erc20Token = new MockMaticToken();
+        erc20Token.mint(OWNER, amountIn);
+
         address[] memory recipients = new address[](2);
         recipients[0] = USER_ONE;
         recipients[1] = USER_TWO;
         swapper.setRecipients(recipients);
-        uint256 amountIn = 6;
-        erc20Token.mint(OWNER, amountIn);
-        stablecoin.mint(address(swapRouter), amountIn);
 
-        vm.startPrank(OWNER);
+        uint256 amountOutPerRecipient = amountIn / recipients.length;
+
+        vm.prank(OWNER);
         erc20Token.approve(address(swapRouter), amountIn);
+        vm.prank(OWNER);
         erc20Token.approve(address(swapper), amountIn);
 
-        swapper.swap(erc20Token, amountIn);
-        vm.stopPrank();
+        vm.expectEmit(true, true, false, true);
+        emit StablecoinSendedToRecipient(recipients[0], amountOutPerRecipient);
+        vm.expectEmit(true, true, false, true);
+        emit StablecoinSendedToRecipient(recipients[1], amountOutPerRecipient);
 
-        uint256 expectedAmount = 3;
-        assertEq(stablecoin.balanceOf(USER_ONE), expectedAmount);
-        assertEq(stablecoin.balanceOf(USER_TWO), expectedAmount);
+        vm.prank(OWNER);
+        swapper.swap(erc20Token, amountIn);
+
+        assertEq(stablecoin.balanceOf(USER_ONE), amountOutPerRecipient);
+        assertEq(stablecoin.balanceOf(USER_TWO), amountOutPerRecipient);
     }
 }
